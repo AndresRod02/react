04d8ed625ac2f89c64cb43fb96d1846c039f736a
Andrés Rodríguez <artdavid2022@gmail.com>
2023-02-23 17:19:28 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import CardSong from './components/CardSong'
-import Button from './components/Button'
+
 
 function App() {
   return (
     <div className="App">
-      <CardSong
-      title = "Storytime" 
-      genero = "Metal sinfonico"
-      banda = "Nightwish"
-      album = "Imaginaerum"
-      fecha = "2011"
-      />
-      <CardSong
-      title = "Flight of Icarus"
-      genero = "Heavy metal"
-      banda = "Iron Maiden"
-      album = "Piece of mind"
-      fecha = "1983"
-      />
-      <div>
-
-      <Button/>
-      <Button/>
       </div>
-    </div>
   )
 }
 
