a3d76bfdc4648813d54da84dd7061a458440e5b7
Andrés Rodríguez <artdavid2022@gmail.com>
2023-03-10 10:53:44 -0500

    si

--- src/components/RickYMorty.jsx
+++ src/components/RickYMorty.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Character from './Character';
 const RickYMorty = () => {
    const [api, setApi] = useState({})
    const [search, setSearch] = useState("")
    const [residents, setResidents] = useState([])
+    const random = Math.floor(Math.random() * (126) + 1)
     useEffect(()=>{
-        axios.get(`https://rickandmortyapi.com/api/location/${Math.floor(Math.random() * (126) + 1)}`)
+        axios.get(`https://rickandmortyapi.com/api/location/${random}`)
         .then((result)=>{ 
             console.log(result.data)
             setApi(result.data)
+        })
+        .catch((error)=> console.error(error))
+        axios.get(`https://rickandmortyapi.com/api/location/${random}`)
+        .then((result)=>{
+            setResidents(result.data?.residents)
         })
         .catch((error)=> console.error(error))
     }, [])
@@ -19,23 +26,23 @@ const RickYMorty = () => {
             setApi(result.data)
         })
         .catch((error)=> console.error(error))
+        axios.get(`https://rickandmortyapi.com/api/location/${search}`)
+        .then((result)=>{
+            setResidents(result.data?.residents)
+        })
+        .catch((error)=> console.error(error))
     }
-    let characters = axios.get(`https://rickandmortyapi.com/api/location/${search}`)
-    .then((result)=>{
-        setResidents(result.data?.results.residents)
-    })
-    .catch((error)=> console.error(error))
     return (
         <div>
         <input type="text" onChange={(e)=>{setSearch((e.target.value))}}/>
         <button onClick={searchId}>Search</button>
         <h1>{api.name}</h1>
         <ul>
-            {residents.map((residents)=>{
-                <li>
+            {residents.map((residents)=>(
+                <li key={residents}>
                     <h3>{residents}</h3>
                 </li>
-            })}
+            ))}
         </ul>
         </div>
     );
