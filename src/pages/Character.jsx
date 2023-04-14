746513255ec05b536f58e17f57cecda0f7524794
Andrés Rodríguez <artdavid2022@gmail.com>
2023-04-14 04:08:18 -0500

    css character Details

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 const Character = () => {
     const [data, setData] = useState({})
     const {name} = useParams()
@@ -10,10 +11,34 @@ const Character = () => {
         .then((resp)=>setData(resp.data))
         .catch((error)=>console.error(error))
     }, [name])
+    const colors = {
+        normal : '#616161',
+        fighting: '#dd5a5a',
+        flying: '#55b6e2',
+        poison: '#5555e2',
+        ground: '#613b13',
+        rock: '#9a9997',
+        bug: '#7c3795',
+        ghost: '#05033f',
+        steel: '#303030',
+        fire: '#e21313',
+        water: '#5683ff',
+        grass: '#48da8f',
+        electric: '#dbe43d',
+        psychic: '#3dbae4',
+        ice: '#07c8f3',
+        dragon: '#f38107',
+        dark: '#111111',
+        fairy: '#ef2fe5',
+        unknown: '#0f4a1f',
+        shadow: '#4a0f41'
+      }
     return (
         <div className='containerDetails'>
-            <img src="/pokeball.png" alt="" className='pokeball' />
+            <img src="/pokeball.png" alt="" className='pokeball'/>
+            <Link to='/characters'>
             <img src="/pokemon.webp" alt="" className='pokemonImg'/>
+            </Link>
             <div className='principalDetails'>
                 <img src={data.sprites?.other.dream_world.front_default} alt="" />
                 <h1 className='pokemon-name'>{data.name}</h1>
@@ -35,14 +60,14 @@ const Character = () => {
                 <h2>Movements</h2>
                 <ul>
                     {data.moves && data.moves.map((move, index) => (
-                        <li key={index}>{move.move.name}</li>
+                        <li key={index} style={{listStyleType: 'none'}}>{move.move.name}</li>
                     ))}
                 </ul>
             </div>
             <div className='typeDetails'>
                 <h2>Types</h2>
                 {data.types?.map((type, index) => (
-                <h3 key={index}>{type.type.name}</h3>
+                <h3 key={index} style={{backgroundColor: colors[type.type.name]}}>{type.type.name}</h3>
                 ))}
             </div>
         </div>
